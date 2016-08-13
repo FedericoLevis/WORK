b1112133d05600c25a072ddef83b2fe025e71302
Federico Levis <flevis2011@gmail.com>
2016-08-13 14:44:11 +0200

    adding files

---
+++ 7/samples/AllSamples.js
@@ -0,0 +1,36 @@
+
+/* =============================================================================================
+   						CONSTANT
+============================================================================================= */
+
+var JSLOG_LEV = 0;
+
+
+var TIP_SORT_FEATURE="<b>Sort Feature: DAFARE</b>";
+
+
+
+/* =============================================================================================
+									FUNCTION
+============================================================================================= */
+
+/**
+* Called when jsu is loaded
+*/
+function jsu_loaded(){
+  // [Optional] Init jslog
+  // jslog_init(JSLOG_LEV);
+	if (isIE() || isFirefox()){
+		// We can show Video also in Popup
+		var select = getElementById2("videoOpt");
+		select.disabled = false;
+	}	
+	manage_par_opt();
+};
+
+
+function onclickSortFeature(event){
+	TipFixedClicked (TIP_SORT_FEATURE,event);
+}
+
+
