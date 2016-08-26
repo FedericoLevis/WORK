839431356c4006f36221e798d29e55a874f0ea83
Federico Levis <flevis2011@gmail.com>
2016-08-27 00:05:10 +0200

    adding files

--- 3/plugin/jsu.js
+++ 3/plugin/jsu.js
@@ -31,6 +31,7 @@ var JSU_PATH_DOC = "https://rawgit.com/FedericoLevis/JSUDoc/master/";
 //var JSU_PATH_ABOUT_IMG = JSU_PATH_IMG +"about/";
 
 var JSU_FREE=true; // used by samples
+var JSU_GA_EN = true; // Enable GoogleAnaltycs
 
 
 
@@ -73,6 +74,16 @@ require([ // First 3 always present
 						} 
    );
 
+//   ONLY for  GOOGLE ANALYTICS -  this part can be removed 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  if (typeof (JSU_GA_EN) != "undefined" && JSU_GA_EN){
+      ga('create', 'UA-83225633-1', 'auto');
+      ga('send', 'pageview');
+  } 
 
 var jsuLoadedTmo = null;   
 
